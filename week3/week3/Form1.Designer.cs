8da3280313c1a7d7049f7c53c984ac5e2b4e81bf
Cz111111 <116494669+Cz111111@users.noreply.github.com>
2024-10-08 16:46:42 +0800

    第三次作业

---
+++ week3/week3/Form1.Designer.cs
@@ -0,0 +1,111 @@
+﻿namespace week3
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// 必需的设计器变量。
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// 清理所有正在使用的资源。
+        /// </summary>
+        /// <param name="disposing">如果应释放托管资源，为 true；否则为 false。</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows 窗体设计器生成的代码
+
+        /// <summary>
+        /// 设计器支持所需的方法 - 不要修改
+        /// 使用代码编辑器修改此方法的内容。
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.btnSelectFile = new System.Windows.Forms.Button();
+            this.richTextBox1 = new System.Windows.Forms.RichTextBox();
+            this.btnBefore = new System.Windows.Forms.Button();
+            this.btnFormat = new System.Windows.Forms.Button();
+            this.btnAfter = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // btnSelectFile
+            // 
+            this.btnSelectFile.Location = new System.Drawing.Point(18, 20);
+            this.btnSelectFile.Name = "btnSelectFile";
+            this.btnSelectFile.Size = new System.Drawing.Size(250, 50);
+            this.btnSelectFile.TabIndex = 0;
+            this.btnSelectFile.Text = "上传文件";
+            this.btnSelectFile.UseVisualStyleBackColor = true;
+            this.btnSelectFile.Click += new System.EventHandler(this.btnSelectFile_Click);
+            // 
+            // richTextBox1
+            // 
+            this.richTextBox1.Location = new System.Drawing.Point(18, 99);
+            this.richTextBox1.Name = "richTextBox1";
+            this.richTextBox1.Size = new System.Drawing.Size(1345, 685);
+            this.richTextBox1.TabIndex = 1;
+            this.richTextBox1.Text = "";
+            // 
+            // btnBefore
+            // 
+            this.btnBefore.Location = new System.Drawing.Point(297, 20);
+            this.btnBefore.Name = "btnBefore";
+            this.btnBefore.Size = new System.Drawing.Size(250, 50);
+            this.btnBefore.TabIndex = 2;
+            this.btnBefore.Text = "统计原始文件数据";
+            this.btnBefore.UseVisualStyleBackColor = true;
+            this.btnBefore.Click += new System.EventHandler(this.btnBefore_Click);
+            // 
+            // btnFormat
+            // 
+            this.btnFormat.Location = new System.Drawing.Point(580, 20);
+            this.btnFormat.Name = "btnFormat";
+            this.btnFormat.Size = new System.Drawing.Size(250, 50);
+            this.btnFormat.TabIndex = 3;
+            this.btnFormat.Text = "格式化";
+            this.btnFormat.UseVisualStyleBackColor = true;
+            this.btnFormat.Click += new System.EventHandler(this.btnFormat_Click);
+            // 
+            // btnAfter
+            // 
+            this.btnAfter.Location = new System.Drawing.Point(858, 20);
+            this.btnAfter.Name = "btnAfter";
+            this.btnAfter.Size = new System.Drawing.Size(250, 50);
+            this.btnAfter.TabIndex = 4;
+            this.btnAfter.Text = "统计格式化文件数据";
+            this.btnAfter.UseVisualStyleBackColor = true;
+            this.btnAfter.Click += new System.EventHandler(this.btnAfter_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(9F, 18F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(1382, 799);
+            this.Controls.Add(this.btnAfter);
+            this.Controls.Add(this.btnFormat);
+            this.Controls.Add(this.btnBefore);
+            this.Controls.Add(this.richTextBox1);
+            this.Controls.Add(this.btnSelectFile);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button btnSelectFile;
+        private System.Windows.Forms.RichTextBox richTextBox1;
+        private System.Windows.Forms.Button btnBefore;
+        private System.Windows.Forms.Button btnFormat;
+        private System.Windows.Forms.Button btnAfter;
+    }
+}
+
---
+++ week3/week3/Form1.cs
@@ -0,0 +1,116 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Text.RegularExpressions;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace week3
+{
+    public partial class Form1 : Form
+    {
+        private string filePath="";
+        private string originalFile = "";
+        private string formattedFile = "";
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void btnSelectFile_Click(object sender, EventArgs e)
+        {
+            OpenFileDialog openFileDialog = new OpenFileDialog
+            {
+                Filter = "C# Files|*.cs",
+                Title = "Select a C# Source File"
+            };
+
+            if (openFileDialog.ShowDialog() == DialogResult.OK)
+            {
+                filePath = openFileDialog.FileName;
+                //AnalyzeAndFormatFile(filePath);
+                MessageBox.Show("文件上传成功！");
+            }
+        }
+
+        private int CountLines(string content)
+        {
+            string[] lines = content.Split(new[] { "\r\n", "\r", "\n" }, StringSplitOptions.None);
+            return lines.Length;
+        }
+
+        private int CountWords(string content)
+        {
+            //string[] words = Regex.Split(content, @"\W+");
+            int wordCount = Regex.Matches(content, @"\w+").Count;
+            return wordCount;
+        }
+
+        private Dictionary<string, int> CountWordFrequencies(string content)
+        {
+            string[] words = Regex.Split(content, @"\W+");
+            Dictionary<string, int> frequencies = new Dictionary<string, int>();
+
+            foreach (var word in words)
+            {
+                if (!string.IsNullOrEmpty(word))
+                {
+                    if (frequencies.ContainsKey(word))
+                    {
+                        frequencies[word]++;
+                    }
+                    else
+                    {
+                        frequencies[word] = 1;
+                    }
+                }
+            }
+
+            return frequencies;
+        }
+
+        private void ShowResults(int originalLineCount, int originalWordCount, int formattedLineCount, int formattedWordCount, Dictionary<string, int> wordCounts)
+        {
+            MessageBox.Show($"Original Line Count: {originalLineCount}\n" +
+                            $"Original Word Count: {originalWordCount}\n" +
+                            $"Formatted Line Count: {formattedLineCount}\n" +
+                            $"Formatted Word Count: {formattedWordCount}\n" +
+                            "Word Frequencies:\n" + string.Join("\n", wordCounts.Select(kvp => $"{kvp.Key}: {kvp.Value}")),
+                            "Analysis Results");
+        }
+
+        private void btnBefore_Click(object sender, EventArgs e)
+        {
+            originalFile = File.ReadAllText(filePath, Encoding.UTF8);
+            Console.Write(originalFile);
+            int originalLineCount = CountLines(originalFile);
+            int originalWordCount = CountWords(originalFile);
+            richTextBox1.Text = $"原始行数: {originalLineCount}\n"+
+                                $"原始单词数: {originalWordCount}\n";
+        }
+
+        private void btnFormat_Click(object sender, EventArgs e)
+        {
+            string withoutComments = Regex.Replace(originalFile, @"//.*", "", RegexOptions.Multiline);
+            string withoutEmptyLines = Regex.Replace(withoutComments, @"\s*[\r\n]+\s*", "", RegexOptions.Multiline);
+
+            formattedFile = withoutEmptyLines;
+            MessageBox.Show("格式化成功！");
+        }
+
+        private void btnAfter_Click(object sender, EventArgs e)
+        {
+            int formattedLineCount = CountLines(formattedFile);
+            int formattedWordCount = CountWords(formattedFile);
+            Console.Write(formattedFile);
+            richTextBox1.Text = $"格式化后行数: {formattedLineCount}\n" +
+                                $"格式化后单词数: {formattedWordCount}\n" +
+                                "单词列表:\n" + string.Join("\n", CountWordFrequencies(formattedFile).Select(kvp => $"{kvp.Key}: {kvp.Value}"));
+        }
+    }
+}
